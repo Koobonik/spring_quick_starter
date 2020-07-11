51b5355ae7c8af003a4a55d5d45908588e51973a
devkoo <kbi960130@naver.com>
2020-07-11 23:31:08 +0900

    이후에 생길 서비스에 사용될 URL

---
+++ src/main/java/com/devkoo/spring/spring/controller/ServiceContoroller.java
@@ -0,0 +1,13 @@
+package com.devkoo.spring.spring.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("service/v1")
+public class ServiceContoroller {
+
+
+}
