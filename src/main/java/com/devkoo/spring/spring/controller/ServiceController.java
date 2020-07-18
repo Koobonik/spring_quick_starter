ed6793e82c5e69034a5b68344d494a681e7351a0
devkoo <kbi960130@naver.com>
2020-07-19 00:47:47 +0900

    기본적인 함수 추가

--- src/main/java/com/devkoo/spring/spring/controller/ServiceController.java
+++ src/main/java/com/devkoo/spring/spring/controller/ServiceController.java
@@ -2,12 +2,18 @@ package com.devkoo.spring.spring.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("service/v1")
 public class ServiceController {
-
+    @RequestMapping(value = "", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody
+    String main()  {
+        return "good page";
+    }
 
 }
