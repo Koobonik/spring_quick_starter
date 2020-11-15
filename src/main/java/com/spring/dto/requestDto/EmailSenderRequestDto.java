28d0fc372a4b6051d8aa4e4c106c8f909fb4ff7b
devkoo <kbi960130@naver.com>
2020-11-15 16:17:00 +0900

    문구 수정

--- src/main/java/com/spring/dto/requestDto/EmailSenderRequestDto.java
+++ src/main/java/com/spring/dto/requestDto/EmailSenderRequestDto.java
@@ -17,7 +17,7 @@ public class EmailSenderRequestDto {
     @ApiModelProperty(example = "mango@naver.com", value = "받는 사람의 이메일 주소", required = true)
     private String recipient;
 
-    @ApiModelProperty(example = "차카지 계정을 활성화 하세요!", value = "이메일 제목", required = true)
+    @ApiModelProperty(example = "계정을 활성화 하세요!", value = "이메일 제목", required = true)
     private String subject;
 
     @ApiModelProperty(example = "이메일 인증 링크를 클릭하여 들어가주세요!", value = "이메일 내용", required = true)
