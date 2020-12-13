97d087f7273063abd75812c90e274fe4d3c8e40a
Koobonik <kbi960130@naver.com>
2020-12-13 18:11:39 +0900

    토큰 사용 여부 결정 코드 추가

--- src/main/java/com/spring/configuration/web/HttpInterceptor.java
+++ src/main/java/com/spring/configuration/web/HttpInterceptor.java
@@ -1,7 +1,9 @@
 package com.spring.configuration.web;
 
 import com.spring.service.PricingPlanService;
+import com.spring.util.jwt.JwtTokenProvider;
 import io.github.bucket4j.Bucket;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,26 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 @Log4j2
+@RequiredArgsConstructor
 public class HttpInterceptor extends HandlerInterceptorAdapter {
 	PricingPlanService pricingPlanService = new PricingPlanService();
+	private  final JwtTokenProvider jwtTokenProvider;
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 							 HttpServletResponse response,
 							 Object handler) {
-		Bucket bucket = pricingPlanService.resolveBucket(request);
+
+		String jwt = request.getHeader("Authorization");
 		log.info("================ Before Method");
 		log.info("접속 ip 주소 '{}'", request.getRemoteAddr());
 		log.info(request.getRemoteAddr());
-
-		if (bucket.tryConsume(1)) { // 1개 사용 요청
-			// 초과하지 않음
-			log.info("초과 안함");
-			return true;
-		} else {
-			// 제한 초과
-			log.info("{} 트래픽 초과!!!", request.getRemoteAddr());
-			return false;
+		if(tokenFilter(request)){
+			if(jwt != null && !jwt.equals("undefined")){
+				Bucket bucket = pricingPlanService.jwtBucket(jwt);
+				log.info("jwt값 : '{}'", jwt);
+				if(!jwtTokenProvider.validateToken(jwt)){
+					log.info("토큰만료 : '{}'", jwt);
+					response.setStatus(401);
+					return false;
+				}
+				return validOverTraffic(bucket, jwt);
+			}
 		}
+		Bucket bucket = pricingPlanService.resolveBucket(request);
+		return validOverTraffic(bucket, request.getRemoteAddr());
 	}
 	@Override
 	public void postHandle( HttpServletRequest request,
@@ -45,5 +54,24 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 								Object handler, 
 								Exception ex) {
 		log.info("================ Method Completed");
+	}
+	private boolean validOverTraffic(Bucket bucket, String data){
+		if (bucket.tryConsume(1)) { // 1개 사용 요청
+			// 초과하지 않음
+			return true;
+		} else {
+			// 제한 초과
+			log.info("{} 트래픽 초과!!!", data);
+			return false;
+		}
+	}
+
+	public boolean tokenFilter(HttpServletRequest re){
+		if(re.getRequestURI().contains("signIn") ||
+				re.getRequestURI().contains("region/city") ||
+				re.getRequestURI().contains("region/region")){
+			return false;
+		}
+		return true;
 	}
 }
--- src/main/java/com/spring/service/PricingPlanService.java
+++ src/main/java/com/spring/service/PricingPlanService.java
@@ -30,16 +30,41 @@ public class PricingPlanService {
             .build();
     }
 
-    
+
+    /* ---------------------- jwt로 제한! ----------------- */
+    public Bucket jwtBucket(String jwt) {
+        return cache.computeIfAbsent(jwt, this::newJwtBucket);
+    }
+
+    private Bucket newJwtBucket(String apiKey){
+        return Bucket4j.builder()
+                // 1000개의 jwt가 10초에 30개씩 보낼 수 있는 대역폭
+                .addLimit(Bandwidth.classic(30, Refill.intervally(1000, Duration.ofSeconds(10))))
+                .build();
+    }
+
+    /* ---------------------- ip주소로 제한! ----------------- */
+    public Bucket remoteAddressBucket(String addr) {
+        return cache.computeIfAbsent(addr, this::newRemoteAddressBucket);
+    }
+
+    private Bucket newRemoteAddressBucket(String apiKey){
+        return Bucket4j.builder()
+                // 1000개의 jwt가 10초에 30개씩 보낼 수 있는 대역폭
+                .addLimit(Bandwidth.classic(30, Refill.intervally(1000, Duration.ofSeconds(10))))
+                .build();
+    }
+
+
     /* ---------------------- sms 문자 제한! ----------------- */
-    public Bucket smsBucket(HttpServletRequest httpServletRequest) {
-        return cache.computeIfAbsent(getHost(httpServletRequest), this::newSmsBucket);
+    public Bucket smsBucket(String callNumber) {
+        return cache.computeIfAbsent(callNumber, this::newSmsBucket);
     }
 
     private Bucket newSmsBucket(String apiKey){
         return Bucket4j.builder()
                 // 2개의 클라이언트가 30초에 10개씩 보낼 수 있는 대역폭
-                .addLimit(Bandwidth.classic(10, Refill.intervally(2, Duration.ofSeconds(30))))
+                .addLimit(Bandwidth.classic(1, Refill.intervally(1, Duration.ofSeconds(30))))
                 .build();
     }
 }
